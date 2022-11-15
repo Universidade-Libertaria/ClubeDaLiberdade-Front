259f6db089eeef8ad28927bebed84e87f6124e4f
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-11-15 17:41:39 -0300

    fix: header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import styles from './Header.module.css'
-import Logo from '../../assets/Logo.png'
+import React from "react";
+import styles from "./Header.module.css";
+import Logo from "../../assets/Logo.png";
 
 export function Header() {
   return (
-        <header>
-            <div className={styles.headerContainer}>
-              <img src={Logo} alt="" />
-              <div className={styles.menuheader}>
-                <nav>
-                  <ul className={styles.NavBar}>
-                    <li><a>Artigos</a></li>
-                    <li><a>Vídeos</a></li>
-                    <li><a>Jornal Libertário</a></li>
-                  </ul>
-                </nav>
-              </div>
-  
-              <div className={styles.btns}>
-                  <button>CLUBE DA LIBERDADE</button>
-                  <button>LOGIN</button>
-              </div>
-            </div>
-        </header>
-  )
+    <header>
+      <div className={styles.headerContainer}>
+        <img src={Logo} alt='' />
+        <div className={styles.menuHeader}>
+          <nav>
+            <ul className={styles.NavBar}>
+              <li>
+                <a>Artigos</a>
+              </li>
+              <li>
+                <a>Vídeos</a>
+              </li>
+              <li>
+                <a>Jornal Libertário</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className={styles.btns}>
+          <button>CLUBE DA LIBERDADE</button>
+          <button>LOGIN</button>
+        </div>
+      </div>
+    </header>
+  );
 }
